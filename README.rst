3ed7f48012c665acfdc8540c605fbb42c45d2362
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2012-01-24 15:00:35 +0200

    directory moved

---
+++ README.rst
@@ -0,0 +1,1 @@
+See `sll/policy/docs/README.rst <https://github.com/hexagonit/sll.policy/blob/master/sll/policy/docs/README.rst>`_
--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
 from setuptools import find_packages
 from setuptools import setup
 
-import os
-
-
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-long_description = (
-    open("README.txt").read() + "\n" +
-    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
-    open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
-    open(os.path.join("docs", "CREDITS.txt")).read()
-)
 
 setup(
     name='sll.policy',
     version='0.6',
     description="Turns plone site into SLL site.",
-    long_description=long_description,
+    long_description=open("README.rst").read(),
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
