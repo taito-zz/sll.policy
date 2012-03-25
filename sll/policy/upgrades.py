76c816669a753e43d0dca9a3a32e6082967a5b70
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-03-26 00:02:20 +0300

    Upgrade step added.

--- sll/policy/upgrades.py
+++ sll/policy/upgrades.py
@@ -13,6 +13,7 @@ def upgrade_1_to_2(context, logger=None):
         logger = logging.getLogger(__name__)
     portal_url = getToolByName(context, 'portal_url')
     portal = portal_url.getPortalObject()
+    logger.info('Start removing unused roles.')
     portal.__ac_roles__ = ('Contributor', 'Member', 'Site Administrator')
     logger.info('Removed unused roles.')
 
@@ -23,6 +24,7 @@ def upgrade_2_to_3(context, logger=None):
         # Called as upgrade step: define our own logger.
         logger = logging.getLogger(__name__)
     groupstool = getToolByName(context, 'portal_groups')
+    logger.info('Start removing Reviewers group.')
     groupstool.removeGroup('Reviewers')
     logger.info('Removed Reviewers group.')
 
@@ -50,17 +52,29 @@ def update_contents(context, paths, logger=None):
             folder = aq_parent(obj)
             path = brain.getPath()
             ## Copy objects
+            message = "Start copying '{0}'.".format(path)
+            logger.info(message)
             objs = folder.manage_copyObjects([brain.id])
+            message = "'{0}' copied.".format(path)
+            logger.info(message)
             ## Past objects
+            message = "Start pasting '{0}'.".format(path)
+            logger.info(message)
             folder.manage_pasteObjects(objs)
+            message = "'{0}' pasted.".format(path)
+            logger.info(message)
             ## Delete objects
+            message = "Start removing '{0}'.".format(path)
+            logger.info(message)
             folder.manage_delObjects([brain.id])
             if (
                 brain.review_state == 'published'
             ) or (
                 brain.review_state == 'visible'
             ):
-               will_be_published_paths.append(path)
+                will_be_published_paths.append(path)
+            message = "'{0}' removed.".format(path)
+            logger.info(message)
 
         wftool = getToolByName(folder, "portal_workflow")
 
@@ -69,15 +83,21 @@ def update_contents(context, paths, logger=None):
             new_path = brain.getPath()
             obj = brain.getObject()
             obj.setId(new_id)
-            if new_path in will_be_published_paths:
-                wftool.doActionFor(obj, 'publish')
-                obj.reindexObject(idxs=['review_state'])
-            obj.reindexObject()
             path = '/'.join(obj.getPhysicalPath())
+            if new_path in will_be_published_paths:
+                message = "Start publishing '{0}'.".format(path)
+                logger.info(message)
+                wftool.doActionFor(obj, 'publish')
+                # obj.reindexObject(idxs=['review_state'])
+                message = "'{0}' published.".format(path)
+                logger.info(message)
+
+            obj.reindexObject()
 
             message = "'{0}' updated.".format(path)
             logger.info(message)
         return paths
+
 
 def upgrade_3_to_4(context, logger=None):
     """"Delete Removable Folder."""
@@ -91,6 +111,7 @@ def upgrade_3_to_4(context, logger=None):
 
     # Remove unnecessary contents
     if portal.get('removable'):
+        logger.info('Start removing Removable Folder.')
         portal.manage_delObjects(['removable'])
         logger.info('Removable Folder Removed.')
 
@@ -103,8 +124,10 @@ def upgrade_4_to_5(context, logger=None):
 
     # First import workflow.xml
     setup = getToolByName(context, 'portal_setup')
+
+    logger.info('Start reimporting workflows.xml.')
     setup.runImportStepFromProfile(PROFILE_ID, 'workflow', run_dependencies=False, purge_old=False)
-    logger.info('Reimported workflows.xml')
+    logger.info('Reimported workflows.xml.')
 
     # Get portal
     portal_url = getToolByName(context, 'portal_url')
@@ -115,6 +138,7 @@ def upgrade_4_to_5(context, logger=None):
         paths = update_contents(context, paths, logger=logger)
     logger.info('Whole Contents Updated.')
 
+
 def upgrade_5_to_6(context, logger=None):
     """"Rebuild catalog."""
     if logger is None:
@@ -122,5 +146,7 @@ def upgrade_5_to_6(context, logger=None):
         logger = logging.getLogger(__name__)
 
     catalog = getToolByName(context, 'portal_catalog')
+
+    logger.info('Start recataloging whole contents.')
     catalog.clearFindAndRebuild()
     logger.info('Whole Contents are now recataloged.')
