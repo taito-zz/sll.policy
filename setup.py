cb988848032ce65975fdea55d2f585b243efdc1b
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-10-16 20:46:14 +0300

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.policy',
-    version='1.1',
+    version='1.1.1',
     description="Turns plone site into SLL site.",
     long_description=open("README.rst").read(),
     # Get more strings from
