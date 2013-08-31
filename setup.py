9cc38d87819922b798fc631834ec90ac93510c75
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-08-31 20:02:30 +0300

    Release 1.8.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.policy',
-    version='1.8',
+    version='1.8.1',
     description="Turns plone site into SLL site.",
     long_description=open("README.rst").read(),
     classifiers=[
