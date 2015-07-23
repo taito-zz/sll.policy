60527a557b15ac20991efe4d285413658fe8367d
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-07-23 16:06:35 +0300

    cosmetics

--- src/sll/policy/browser/interfaces.py
+++ src/sll/policy/browser/interfaces.py
@@ -3,7 +3,3 @@ from zope.interface import Interface
 
 class ISllPolicyLayer(Interface):
     """Marker interface for browserlayer."""
-
-
-# class ITopPageFeed(Interface):
-#     """Marker interface for top page feed."""
